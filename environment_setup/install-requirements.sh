92ab04a1da7b9922ee4171085193b09705e76f8e
Deepak Sharma <81965331+dpkitworks@users.noreply.github.com>
2021-07-14 17:24:14 +0530

    changes

--- environment_setup/install-requirements.sh
+++ environment_setup/install-requirements.sh
@@ -1,8 +1,8 @@
 python --version
+pip install --upgrade azure-mgmt-resource
 pip install --upgrade azure-cli
 #pip uninstall azure-mgmt-resource
 #pip install azure-mgmt-resource==15.0.0
-pip install --upgrade azure-mgmt-resource
 pip install --upgrade azureml-sdk[cli]
 pip install pytest
 pip install pytest-cov
